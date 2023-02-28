215d31dc78962feb18fd8976dd32c038044c323d
Roma1717 <rzavtonev@gmail.com>
2023-02-28 20:18:47 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //int[] arr = new int[10];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //Console.Write($"Введите значение для элемента {i}: ");
+            //int value = int.Parse(Console.ReadLine());
+            //arr.SetValue(value, i);
+            //}
+            //Console.WriteLine("Массив:");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    int value = (int)arr.GetValue(i);
+            //    Console.WriteLine($"Элемент {i}: {value}");
+            //}
+            //Задание 2 
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Array.Clear(arr, 1, 3);
+            //foreach (int num in arr)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //Задача 3
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            //Array.Reverse(arr);
+            //foreach (int element in arr)
+            //{
+            //    Console.Write(element + " ");
+            //}
+            //Задание 4
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Array.Resize(ref arr, 6);
+            //foreach (int element in arr)
+            //{
+            //    Console.Write(element + " ");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
